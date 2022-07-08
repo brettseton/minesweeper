9c1ea9c2f3d25571485c4270a55b7ea99305d23b
brett.seton <brett.seton@blueshift.one>
2022-07-08 17:45:47 +1000

    added simple backend benchmark

---
+++ src/backend/dotnet/tests/benchmarks/benchmarks/GameControllerBenchmarks.cs
@@ -0,0 +1,19 @@
+﻿using backend.Controllers;
+using backend.Repository;
+using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.Logging;
+
+namespace benchmarks
+{
+    [MemoryDiagnoser]
+    public class GameControllerBenchmarks
+    {
+        private static readonly GameController _gameController = new(new LoggerFactory(), new InMemoryGameRepository(new LoggerFactory()));
+
+        [Benchmark]
+        public void CreateNewGame()
+        {
+            var result = _gameController.New();
+        }
+    }
+}
